e71ff2209a9d7b59bba34690db8a5d6a50ce6949
michael-franke <the.secret.goldfish@me.com>
2022-08-02 10:13:40 +0200

    linted version, bug fixes

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,79 @@
 <template>
-<Experiment title="magpie demo">
-  <!-- <InstructionScreen :title="'Welcome'"> -->
-  <!--   This is a sample introduction screen. -->
-  <!-- </InstructionScreen> -->
+  <Experiment title="magpie demo">
+    <InstructionScreen :title="'Welcome'">
+      In this short experiment you will first read a description about a
+      fictitious context in which you are supposed to make an decision based on
+      some information you received from others. Please read the context
+      information very carefully and choose deliberately.
+      <p></p>
+      After your choice, we will ask you to explain your choice in a few words.
+    </InstructionScreen>
 
-  <template v-for="(trial, i) of items">
-    <Screen>
-
-      <Slide>
-        <p><strong>Context:</strong> {{trial.context}}</p>
-        <p><strong>{{trial.criticalSentence}}</strong></p>
-        <p><strong>Question:</strong> {{trial.taskQuestion}}</p>
-        <ForcedChoiceInput
-          :response.sync= "$magpie.measurements.response"
-          :options="['both xeliherb and ralocrop', 'bnly xeliherb']"
-          @update:response="$magpie.saveAndNextScreen();"
+    <template v-for="(trial, i) of items">
+      <Screen :key="i">
+        <Slide>
+          <p><strong>Context:</strong> {{ trial.context }}</p>
+          <p>
+            <strong>{{ trial.criticalSentence }}</strong>
+          </p>
+          <p><strong>Question:</strong> {{ trial.taskQuestion }}</p>
+          <ForcedChoiceInput
+            :response.sync="$magpie.measurements.response"
+            :options="['both xeliherb and ralocrop', 'only xeliherb']"
+            @update:response="$magpie.saveAndNextScreen()"
           />
-        <Record :data="{
-                       itemNr : trial.itemNr,
-                       itemName : trial.itemName,
-                       condition : trial.condition
-            }" />
-
-      </Slide>
-
-    </Screen>
-
-    <Screen>
-
-      <Slide>
-        <p>Please give a short explanation for why you chose to cultivate {{getPreviousResponse()}}!</p>
-        <p style="color:grey">(You need to enter at least 20 characters of text to proceed.)</p>
-        <TextareaInput
-            :response.sync= "$magpie.measurements.justification"
+          <Record
+            :data="{
+              trialNR: i,
+              itemNr: trial.itemNr,
+              itemName: trial.itemName,
+              condition: trial.condition
+            }"
           />
-        <button v-if= "$magpie.measurements.justification && $magpie.measurements.justification.length > 20" @click="$magpie.saveAndNextScreen();">Submit</button>
-        <Record :data="{
-                       itemNr : trial.itemNr,
-                       itemName : trial.itemName,
-                       condition : trial.condition
-            }" />
-      </Slide>
-    </Screen>
+        </Slide>
+      </Screen>
 
-  </template>
-  <SubmitResultsScreen />
+      <Screen :key="i">
+        <Slide>
+          <p>
+            Please give a short explanation for why you chose to cultivate
+            {{ getPreviousResponse() }}!
+          </p>
+          <p style="color: grey">
+            (You need to enter at least 20 characters of text to proceed.)
+          </p>
+          <TextareaInput :response.sync="$magpie.measurements.justification" />
+          <button
+            v-if="
+              $magpie.measurements.justification &&
+              $magpie.measurements.justification.length > 20
+            "
+            @click="$magpie.saveAndNextScreen()"
+          >
+            Submit
+          </button>
+          <Record
+            :data="{
+              itemNr: trial.itemNr,
+              itemName: trial.itemName,
+              condition: trial.condition
+            }"
+          />
+        </Slide>
+      </Screen>
+    </template>
+    <SubmitResultsScreen />
   </Experiment>
 </template>
 
 <script>
-
 import items from '../trials/items.csv';
 import _ from 'lodash';
 
 export default {
   name: 'App',
   data() {
-    return {items: _.shuffle(items).slice(0,1)};
+    return { items: _.shuffle(items).slice(0, 1) };
   },
   computed: {
     // Expose lodash to template code
@@ -65,8 +82,8 @@ export default {
     }
   },
   methods: {
-    getPreviousResponse : function() {
-      return(this.$magpie.trialData[0][0].response)
+    getPreviousResponse: function () {
+      return this.$magpie.trialData[0][0].response;
     }
   }
 };

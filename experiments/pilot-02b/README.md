c60c32b43891304b07d523e8d59b48f90d844924
michael-franke <the.secret.goldfish@me.com>
2022-10-30 15:43:49 +0100

    added data Pilot 2b; moved 2b to 'experiments'

--- experiments/pilot-02b/README.md
+++ experiments/pilot-02b/README.md
@@ -2,6 +2,13 @@
 
 [Magpie](https://magpie-experiments.org/) experiment for a simple experiment into "clausal implicatures", i.e., inferring causal information where none was explicitly conveyed.
 
-ahis is version **Pilot 02b**
+This is version **Pilot 02b**. This version was a genuine dynamic (iterated) experiment.
+The goal was to run 5 chains with ca. 5 generations.
+
+There were some issues, though:
+
+1. Three runs only submitted partial data (prolific_pids %in% c("62b0d20142d2ad90afb8ba9b", "614b11e399369a0d52b5fbf6", "604fa685e33606f9a0ee8189"))
+2. There were double allocations to the same variant-chain-generation triple.
+3. The reproductions were all very direct "recall" statements. The summary / helpfulness character of the reproductions from Pilot 2a seems to have been lost.
 
 Live version of the experiment [here](https://magpie-ea.github.io/magpie3-causal-implicature/experiments/pilot-02b).
--- experiments/pilot-02b/src/App.vue
+++ experiments/pilot-02b/src/App.vue
@@ -9,6 +9,11 @@
     After your choice, we will ask you to explain your choice in a few words.
   </InstructionScreen>
 
+    <ConnectInteractiveScreen :title="'Connecting...'"></ConnectInteractiveScreen>
+    <AwaitIteratedResultScreen :title="'Waiting for previous participant to finish'">
+    </AwaitIteratedResultScreen>
+    <Wait :time="0" @done="prepareData();$magpie.nextScreen()" />
+
   <template v-for="(trial, i) of items">
     <Screen :key="i">
       <Slide>
@@ -20,7 +25,10 @@
 
           <p>On arrival to Xelifan-3 you find a message left by earlier colonists. Here is what the message says:</p>
 
-          <div style="background-color:#AAAAFF"><strong>Message from earlier colonists:</strong> <br> A high yield of xeliherb is associated with the presence of ralocrop.
+          <div style="background-color:#AAAAFF">
+            <strong>Message from earlier colonists:</strong>
+            <br>
+            {{ getPreviousResponse() }}
           </div>
 
           <p><strong>Question:</strong> Based on the information provided by Mission Control and the earlier colonists, will you cultivate both xeliherb and ralocrop on the fields available to your colony, or will you only cultivate xeliherb?</p>
@@ -45,7 +53,7 @@
         <Slide>
           <p>
             Please give a short explanation for why you chose to cultivate
-            {{ getPreviousResponse() }}!
+            {{ getChoice() }}!
           </p>
           <!-- <p style="color: grey"> -->
           <!--   (You need to enter at least 20 characters of text to proceed.) -->
@@ -73,15 +81,24 @@
 
       <Screen :key="i">
         <Slide>
+          <h2><strong>Oh, no! Abort mission immediately!</strong></h2>
           <p>
             Due to atmospheric conditions and technical problems you are forced to leave Xelifan-3 in a rush.
             You should make sure that the information you received from the earlier colonists is not lost to whoever may come next.
-            Please write down what you recall from the earlier colonists message into the text box!
+            <strong>Please write down what you recall from the earlier colonists' message into the text box!</strong>
           </p>
           <!-- <p style="color: grey"> -->
           <!--   (You need to enter at least 20 characters of text to proceed.) -->
           <!-- </p> -->
           <TextareaInput :response.sync="$magpie.measurements.response" />
+
+          <p>
+          For clarity, you do NOT have to repeat the information received from Mission Control, which is repeated below for your convenience.
+          Only reproduce the message you received from the earlier colonists.
+          </p>
+
+        <div style="background-color:#FF9999"><strong>Information from Mission Control:</strong> <br> Two plants grow exclusively on Xelifan-3. They are called xeliherb and ralocrop. To survive on Xelifan-3, you need constant supply of the seeds of xeliherb. The cultivation of ralocrop is costly (water, energy resources).</div>
+
           <button
             v-if="
               $magpie.measurements.response
@@ -90,6 +107,7 @@
           >
             Submit
           </button>
+
           <Record
             :data="{
                    trialNR: i+2,
@@ -114,7 +132,7 @@
 import items from '../trials/items.csv';
 import _ from 'lodash';
 
-console.log("Hi, I'm Pilot 02b!")
+console.log("Hi, I'm Pilot 02b, embedded!")
 
 export default {
   name: 'App',
@@ -129,8 +147,49 @@ export default {
     }
   },
   methods: {
-    getPreviousResponse: function () {
-      return this.$magpie.trialData[1][0].response;
+    getChoice: function () {
+      // we need index 4 here because this is the 4th trial in the experiment overall!?
+      var output = this.$magpie.trialData[4] == null? null : this.$magpie.trialData[4][0].response;
+      return (output);
+
+    },
+    prepareData: function(){
+      console.log("variant: ", this.$magpie.socket.variant)
+      console.log("chain: ", this.$magpie.socket.chain)
+      console.log("generation: ", this.$magpie.socket.generation)
+      var variant = this.$magpie.socket.variant
+      this.guessingTrials = _.shuffle(_.filter(this.guessingTrials, {'variant' : variant}))
+      this.$magpie.addExpData({'variant': this.$magpie.socket.variant,
+                               'generation': this.$magpie.socket.generation,
+                               'chain': this.$magpie.socket.chain,
+                               'triggerMessage' : this.getPreviousResponse()})
+    },
+    getPreviousResponse: function(){
+
+      // which generation is this
+      var generation = this.$magpie.socket.generation;
+
+      var output = "A high yield of xeliherb is associated with the presence of ralocrop."
+
+      // if we are in the first generation, we will return the defaultGuess for the item
+      if (generation > 1) {
+
+         // get information about the results from the last iteration from the socket
+         var lastIterationResults = this.$magpie.socket.lastIterationResults;
+
+         // before initialization with ConnectInteractiveScreen, lastIterationResults will be 'null'
+         // so we make sure that there is some output
+         // if we are neither first-generation in the chain, nor have an empty socket,
+         // we can return the previous input for the current item
+
+         var filteredData = _.filter(lastIterationResults, function(o) {return o.measure == 'reproduction'})
+         var extractedRow = filteredData[0]
+         // console.log(filteredData)
+         // console.log(extractedRow)
+         output = extractedRow == null ? null : extractedRow.response
+      }
+
+      return( output )
     }
   }
 };

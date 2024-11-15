3ae422073b0caba60193c7e0bf7ec26393b0f0c2
Hening Wang <hening.wang0615@gmail.com>
2024-11-15 13:19:47 +0100

    add prolific info to config

--- experiments/pilot-indirectSource/src/magpie.config.js
+++ experiments/pilot-indirectSource/src/magpie.config.js
@@ -2,9 +2,9 @@ export default {
   experimentId: '4',
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C13NOKGZ',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CHFFTKHZ',
   contactEmail: 'hening.wang@outlook.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };

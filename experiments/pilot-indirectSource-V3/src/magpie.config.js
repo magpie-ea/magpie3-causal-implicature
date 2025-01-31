4fc35271cd8234be47d6c4e35948ce19355d60ff
Hening Wang <hening.wang0615@gmail.com>
2025-01-31 17:22:11 +0100

    change prolifc completion code for p3

--- experiments/pilot-indirectSource-V3/src/magpie.config.js
+++ experiments/pilot-indirectSource-V3/src/magpie.config.js
@@ -2,7 +2,7 @@ export default {
   experimentId: '5',
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1DUEH9N',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1KQOFVX',
   contactEmail: 'hening.wang@outlook.com',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',

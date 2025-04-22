57a43da6b78414aa49f3e478e64994609e54c3d1
Hening Wang <hening.wang0615@gmail.com>
2025-04-22 21:52:43 +0200

    setup for other two factors of speaker relevance

---
+++ experiments/stable-indirectSource-V2/src/magpie.config.js
@@ -0,0 +1,10 @@
+export default {
+  experimentId: '5',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CG0M23XI',
+  contactEmail: 'hening.wang@outlook.com',
+  // Either 'debug', 'directLink' or 'prolific'
+  mode: 'prolific',
+  language: 'en'
+};
---
+++ experiments/stable-listenerRelevance/README.md
@@ -0,0 +1,4 @@
+# Pilot 'indirect sources'
+
+
+Live version of the experiment [here](https://magpie-ea.github.io/magpie3-causal-implicature/experiments/pilot-indirectSource/).

7ccaf85840b5c199480eebfaec78b39e07fa6e4b
Hening Wang <hening.wang0615@gmail.com>
2025-05-21 15:26:05 +0200

    upload analysis with all 4 conditions

--- experiments/stable-indirectSource-V2/src/App.vue
+++ experiments/stable-indirectSource-V2/src/App.vue
@@ -250,8 +250,8 @@ export default {
       // this is random
       //items: _.shuffle(items).slice(0, 1),
       //select the first element of items
-      items: items.slice(1,2), // this is indirect
-      //items: items.slice(0,1), // this is direct
+      //items: items.slice(1,2), // this is indirect
+      items: items.slice(0,1), // this is direct
       // select the second element of items
       // items: items[1],
     selectedTrial: null,
